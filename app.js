efd24c8fccb20bd5542257b9f4e5ff07d5167580
Mayank Gaba <cu.17bcs1192@gmail.com>
2020-05-24 02:27:43 +0530

    started with writing the API

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHandler = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/',(req, res, next) => {
-    res.send('Hello Mayank');
-})
+app.use('/graphqlApi',graphqlHandler({
+    schema: buildSchema(`
+        type queryTypes {
+            events : [String!]!
+        }
+
+        type mutationTypes {
+            createEvent(name: String): String 
+        }
+
+        schema {
+            query : queryTypes 
+            mutation : mutationTypes
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic','Game me','fuck you bitch'] 
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
